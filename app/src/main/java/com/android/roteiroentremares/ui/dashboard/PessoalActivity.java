1338ee3d8a59f58b6f8562520a877830ca5265f9
gasparrmg <rafael.gaspar97@gmail.com>
2021-01-29 16:48:53 +0000

    Refactored Bottom Navigation into Drawer Navigation.

--- app/src/main/java/com/android/roteiroentremares/ui/dashboard/PessoalActivity.java
+++ app/src/main/java/com/android/roteiroentremares/ui/dashboard/PessoalActivity.java
@@ -1,29 +1,27 @@
-package com.android.roteiroentremares.ui.dashboard.screens;
+package com.android.roteiroentremares.ui.dashboard;
 
+import android.app.ActionBar;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.roteiroentremares.R;
 import com.android.roteiroentremares.ui.dashboard.viewmodel.DashboardViewModel;
+import com.android.roteiroentremares.ui.onboarding.MainActivity;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
-import com.mobsandgeeks.saripaar.annotation.Checked;
 import com.mobsandgeeks.saripaar.annotation.Length;
 import com.mobsandgeeks.saripaar.annotation.Max;
 import com.mobsandgeeks.saripaar.annotation.Min;
@@ -32,16 +30,15 @@ import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.List;
-import java.util.UUID;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
-public class PessoalFragment extends Fragment implements Validator.ValidationListener {
+public class PessoalActivity extends AppCompatActivity implements Validator.ValidationListener {
 
-    private static String TAG = "PESSOAL_FRAGMENT";
+    private static String TAG = "PESSOAL_ACTIVITY";
 
     // ViewModel
     @Inject
@@ -88,22 +85,21 @@ public class PessoalFragment extends Fragment implements Validator.ValidationLis
     private String anoLectivoUtilizador;
     private String code;
 
-
-    public PessoalFragment() {
-        // Required empty public constructor
-    }
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_pessoal, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pessoal);
+        getSupportActionBar().setTitle(R.string.title_pessoal);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        dashboardViewModel.setTipoUtilizador(1); // Uncomment ONLY for testing purposes
+        // Keeps keyboard from opening right away
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        //dashboardViewModel.setTipoUtilizador(1); // Uncomment ONLY for testing purposes
 
         getUserInfo();
 
-        initViews(view);
+        initViews();
         setOnClickListeners();
 
         initForm();
@@ -111,8 +107,6 @@ public class PessoalFragment extends Fragment implements Validator.ValidationLis
         // Init Form Validator
         formValidator = new Validator(this);
         formValidator.setValidationListener(this);
-
-        return view;
     }
 
     private void getUserInfo() {
@@ -124,21 +118,21 @@ public class PessoalFragment extends Fragment implements Validator.ValidationLis
         code = dashboardViewModel.getCodigoTurma();
     }
 
-    private void initViews(View view) {
-        editTextNome = view.findViewById(R.id.textinputedittext_nome);
-        textInputLayoutEscola = view.findViewById(R.id.textinputlayout_escola);
-        editTextEscola = view.findViewById(R.id.textinputedittext_escola);
-        textInputLayoutAnoEscolaridade = view.findViewById(R.id.textinputlayout_anoescolaridade);
-        editTextAnoEscolaridade = view.findViewById(R.id.textinputedittext_anoescolaridade);
-        textInputLayoutAnoLectivo = view.findViewById(R.id.textinputlayout_anolectivo);
-        editTextAnoLectivo = view.findViewById(R.id.textinputedittext_anolectivo);
-        textInputLayoutCodigoTurma = view.findViewById(R.id.textinputlayout_codigoturma);
-        editTextCodigoTurma = view.findViewById(R.id.textinputedittext_codigoturma);
-        textInputLayoutGerarCodigoTurma = view.findViewById(R.id.textinputlayout_gerarcodigoturma);
-        editTextGerarCodigoTurma = view.findViewById(R.id.textinputedittext_gerarcodigoturma);
-        btnGerarCodigoTurma = view.findViewById(R.id.btn_gerarcodigoturma);
-        btnSave = view.findViewById(R.id.btn_save);
-        btnCopiarCodigoTurma = view.findViewById(R.id.btn_copiarcodigoturma);
+    private void initViews() {
+        editTextNome = findViewById(R.id.textinputedittext_nome);
+        textInputLayoutEscola = findViewById(R.id.textinputlayout_escola);
+        editTextEscola = findViewById(R.id.textinputedittext_escola);
+        textInputLayoutAnoEscolaridade = findViewById(R.id.textinputlayout_anoescolaridade);
+        editTextAnoEscolaridade = findViewById(R.id.textinputedittext_anoescolaridade);
+        textInputLayoutAnoLectivo = findViewById(R.id.textinputlayout_anolectivo);
+        editTextAnoLectivo = findViewById(R.id.textinputedittext_anolectivo);
+        textInputLayoutCodigoTurma = findViewById(R.id.textinputlayout_codigoturma);
+        editTextCodigoTurma = findViewById(R.id.textinputedittext_codigoturma);
+        textInputLayoutGerarCodigoTurma = findViewById(R.id.textinputlayout_gerarcodigoturma);
+        editTextGerarCodigoTurma = findViewById(R.id.textinputedittext_gerarcodigoturma);
+        btnGerarCodigoTurma = findViewById(R.id.btn_gerarcodigoturma);
+        btnSave = findViewById(R.id.btn_save);
+        btnCopiarCodigoTurma = findViewById(R.id.btn_copiarcodigoturma);
 
         editTextAnoLectivo.addTextChangedListener(new TextWatcher() {
             @Override
@@ -192,11 +186,11 @@ public class PessoalFragment extends Fragment implements Validator.ValidationLis
         btnCopiarCodigoTurma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                 ClipData clip = ClipData.newPlainText("códigoTurma", editTextGerarCodigoTurma.getText().toString());
                 clipboard.setPrimaryClip(clip);
 
-                Toast.makeText(getActivity(), "Código de Turma copiado!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(PessoalActivity.this, "Código de Turma copiado!", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -208,6 +202,7 @@ public class PessoalFragment extends Fragment implements Validator.ValidationLis
         if (tipoUtilizador == 0) {
             textInputLayoutGerarCodigoTurma.setVisibility(View.GONE);
             btnGerarCodigoTurma.setVisibility(View.GONE);
+            btnCopiarCodigoTurma.setVisibility(View.GONE);
         } else if (tipoUtilizador == 1) {
             textInputLayoutCodigoTurma.setVisibility(View.GONE);
         } else {
@@ -230,8 +225,7 @@ public class PessoalFragment extends Fragment implements Validator.ValidationLis
             if (!code.isEmpty()) {
                 editTextCodigoTurma.setText(code);
                 editTextGerarCodigoTurma.setText(code);
-                btnGerarCodigoTurma.setVisibility(View.INVISIBLE);
-                btnCopiarCodigoTurma.setVisibility(View.VISIBLE);
+                btnGerarCodigoTurma.setVisibility(View.GONE);
             }
         }
     }
@@ -256,7 +250,7 @@ public class PessoalFragment extends Fragment implements Validator.ValidationLis
             }
         }
 
-        Toast.makeText(getActivity(), "Informações guardadas com sucesso!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(PessoalActivity.this, "Informações guardadas com sucesso!", Toast.LENGTH_SHORT).show();
 
         getUserInfo();
         btnSave.setEnabled(false);
@@ -266,14 +260,14 @@ public class PessoalFragment extends Fragment implements Validator.ValidationLis
     public void onValidationFailed(List<ValidationError> errors) {
         for (ValidationError error : errors) {
             View view = error.getView();
-            String message = error.getCollatedErrorMessage(getContext());
+            String message = error.getCollatedErrorMessage(PessoalActivity.this);
 
             // Display error messages
             if (view instanceof TextInputEditText) {
                 ((TextInputEditText) view).setError(message);
                 // ((TextInputLayout) view.getParent().getParent()).setError(message);
             } else {
-                Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+                Toast.makeText(PessoalActivity.this, message, Toast.LENGTH_LONG).show();
             }
         }
     }
